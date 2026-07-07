5a5093a540e0e3faaebbe63ba43921d1178f8e08
Adonay Tesfamichael <adonayaraia02@gmail.com>
2026-07-07 13:57:54 -0400

    header is able to fetch data and project it. UI is also cleaned up

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
-// import ProductSkeleton from "./ui/ProductSkeleton";
 // import { AppContext } from "../context/AppContext";
 import Product from "./ui/Product";
 import axios from "axios";
+import ProductSkeleton from "./ui/ProductSkeleton";
+
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -23,9 +24,16 @@ const Products = () => {
         <div className="row products__row">
           <h2 className="products__title">Products we are proud of</h2>
           <div className="products__list">
-            {products.slice(0, 8).map((product) => (
-              <Product key={product.id} product={product} />
-            ))}
+            {products.length > 0 ? (
+              products
+                .slice(0, 8)
+                .map((product) => (
+                  <Product key={product.id} product={product} />
+                ))
+            ) : (
+              new Array(8).fill().map(()=>
+                <ProductSkeleton />)
+            )}
           </div>
         </div>
       </div>
