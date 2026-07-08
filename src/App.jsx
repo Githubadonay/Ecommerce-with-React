8dc6d5da59e5dcd150694ce57ec9853b4089eaa7
Adonay Tesfamichael <adonayaraia02@gmail.com>
2026-07-07 22:25:37 -0400

    fixed mistakes and finished the home page look, added JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import Nav from "./components/Nav"
-import Header from "./components/Header"
-import Product from "./components/Products";
+import React, { useEffect, useState } from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Products from "./components/Products";
 import Banner1 from "./components/Banner1";
+import TrendingProducts from "./components/TrendingProducts";
+import { AppContext } from "./context/AppContext";
+import axios from "axios";
+import Banner2 from "./components/Banner2";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  async function fetchProducts() {
+    const { data } = await axios.get(
+      "https://ecommerce-samurai.up.railway.app/product",
+    );
+    const productsData = data.data;
+    setProducts(productsData);
+    console.log(products);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
-    <>
-    <Nav />
-    <Header/>
-    <Product/>
-    <Banner1/>
-    </>
-  )
+    <AppContext.Provider value={{ products }}>
+      <Nav />
+      <Header />
+      <Products />
+      <Banner1 />
+      <TrendingProducts />
+      <Banner2 />
+      <Newsletter />
+      <Footer />
+    </AppContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,39 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
-// import { AppContext } from "../context/AppContext";
+import React, { useContext } from "react";
 import Product from "./ui/Product";
-import axios from "axios";
 import ProductSkeleton from "./ui/ProductSkeleton";
-
+import { AppContext } from "../context/AppContext";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  async function fetchProducts() {
-    const { data } = await axios.get(
-      "https://ecommerce-samurai.up.railway.app/product",
-    );
-    const productsData = data.data;
-    setProducts(productsData);
-  }
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
+  const {products} = useContext(AppContext)
   return (
     <section id="products">
       <div className="container">
         <div className="row products__row">
           <h2 className="products__title">Products we are proud of</h2>
           <div className="products__list">
-            {products.length > 0 ? (
-              products
-                .slice(0, 8)
-                .map((product) => (
-                  <Product key={product.id} product={product} />
-                ))
-            ) : (
-              new Array(8).fill(0).map(()=>
-                <ProductSkeleton />)
-            )}
+            {products.length > 0
+              ? products
+                  .slice(0, 8)
+                  .map((product) => (
+                    <Product key={product.id} product={product} />
+                  ))
+              : new Array(8).fill(0).map((_,index) => <ProductSkeleton key={index}/>)}
           </div>
         </div>
       </div>
